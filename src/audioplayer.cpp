12bebbce41daaa7b115510c4adee05bfa8677bea
Lang <transplus@foxmail.com>
2023-08-17 19:25:27 +0800

    New functionality(AudioPlayer): A wrapper to PortAudioX, which provide a simple async interface to simplify the routine to play audio(PCM data)

---
+++ src/audioplayer.cpp
@@ -0,0 +1,84 @@
+﻿#include "audioplayer.h"
+#include "portaudiox.h"
+
+#include <assert.h>
+#include <qDebug>
+
+AudioPlayer::AudioPlayer()
+  : m_state(STATE_NOT_INITED),
+    m_audio_out(new PortAudioX(PortAudioX::MODE_OUTPUT))
+{
+    init();
+}
+
+AudioPlayer::~AudioPlayer()
+{
+    // default dtor is okay
+}
+
+void AudioPlayer::queueToPlay(void *audio_src, int src_bytes)
+{
+    std::lock_guard<std::mutex> lock(pcm_mutex);
+    assert(src_bytes > 0);
+    std::copy((char*)audio_src, (char*)audio_src + src_bytes, std::back_inserter(pcm_data));
+}
+
+void AudioPlayer::init()
+{
+    // allready inited, skip
+    if(m_state != STATE_NOT_INITED){
+        return ;
+    }
+
+    m_audio_out->setOnAudioWritableCallback([this](void* output, const unsigned long frameCountLimit,
+                                                   size_t nBytesPerFrame, uint8_t n_channel,
+                                                   const PaStreamCallbackTimeInfo* timeInfo){
+        // you can only paly nBytesLimit
+        int nBytesLimit = frameCountLimit * nBytesPerFrame * n_channel;
+
+        {
+            std::lock_guard<std::mutex> lock(pcm_mutex);
+
+            if(pcm_data.size() < nBytesLimit){
+                return paContinue;
+            }
+            else{
+                std::copy(pcm_data.begin(), pcm_data.begin() + nBytesLimit, (uint8_t*)output);
+                pcm_data.erase(pcm_data.begin(), pcm_data.begin() + nBytesLimit);
+            }
+        }
+
+        return paContinue;
+    });
+
+    m_state = STATE_INITED;
+}
+
+bool AudioPlayer::start()
+{
+    if(m_state != STATE_INITED){
+        return false;
+    }
+    // clear ouput buffer
+    pcm_data.clear();
+    if(m_audio_out->start()){
+        m_state = STATE_STARTED;
+        return true;
+    }
+    // can`t start m_audio_out
+    return false;
+}
+
+bool AudioPlayer::stop()
+{
+    if(m_state != STATE_STARTED){
+        return false;
+    }
+
+    if( m_audio_out->stopRightNow() ){
+        m_state = STATE_STOPPED;
+        return true;
+    }
+    // can`t stop m_audio_out
+    return false;
+}
---
+++ src/audioplayer.h
@@ -0,0 +1,56 @@
+﻿/*************************************************************************
+*    File Name: audioplayer.h
+*    Description: This class is basically a wrapper to PortAudioX(MODE_OUTPUT)
+*                 But provide a simple async interface to simplify the routine
+*                 to play audio
+*    Author: Lance @ npwu
+*    Email: transplus@foxmail.com
+*    Create Time: 2023-08-17
+*    License: GPL license, and For learning use only, not for commercial use.
+*    Other Info: None
+*    Modification Info: None
+*************************************************************************/
+#ifndef AUDIOPLAYER_H
+#define AUDIOPLAYER_H
+
+
+#include <deque>
+#include <memory>
+#include <mutex>
+
+class PortAudioX;
+
+class AudioPlayer
+{
+public:
+    AudioPlayer();
+    ~AudioPlayer(); // force outline
+
+    // MT-Safe
+    // Queue audio to audio playing queue
+    // FIXME: if the client invoke queueToPlay too fast, the internal buffer may consume to much memory
+    void queueToPlay(void *audio_src, int src_bytes);
+
+    bool start();
+
+    bool stop();
+
+private:
+    void init();
+
+    enum INTERNAL_STATE{
+        STATE_NOT_INITED,
+        STATE_INITED,
+        STATE_STARTED,
+        STATE_STOPPED,
+    };
+    // interanl state machine to prevent public interface misuse
+    INTERNAL_STATE m_state = STATE_NOT_INITED;
+
+    std::mutex pcm_mutex;          // protects pcm_data
+    std::deque<uint8_t> pcm_data; // audio paly queue
+
+    std::unique_ptr<PortAudioX> m_audio_out;
+};
+
+#endif // AUDIOPLAYER_H
